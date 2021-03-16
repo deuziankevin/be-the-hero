7b0d1f4be79dc9f688a52e28cf632a6d0b3fb6f2
Kevin Calheiros <deuziankevin@outlook.com>
2021-03-16 19:45:47 -0400

    Front-end & Back-end do App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './global.css'
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Mundo
-        </p>
-        <a
-          className="App-link"
-          href="https://hashtag.blogfolha.uol.com.br/files/2019/06/bolsonaro-rainha-meme2.jpg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          link
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
